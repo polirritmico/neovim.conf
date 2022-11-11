1324d94ba99c1b4647e248b65428aa3c99b305cc
polirritmico <ebray187@gmail.com>
2022-11-11 19:14:45 -0300

    Added __pycache__ and venv to ignore telescope

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -1,9 +1,15 @@
 -- Telescope
 require("telescope").setup({
+    defaults = {
+        file_ignore_patterns = {
+            "venv",
+            "__pycache__"
+        }
+    },
     extensions = {
         ["fzf"] = {
             fuzzy = true,
-            override_generic_sorter = true
+            override_generic_sorter = true,
         }
     },
 })
