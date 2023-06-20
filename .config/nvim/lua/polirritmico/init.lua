83a0c1e7f56c68bae21030ddc3255b2e6b425809
polirritmico <ebray187@gmail.com>
2023-06-20 01:08:03 -0400

    Update nvim config

--- .config/nvim/lua/polirritmico/init.lua
+++ .config/nvim/lua/polirritmico/init.lua
@@ -1,3 +1,6 @@
+-- Set global config_path
+MyConfigPath = " $XDG_CONFIG_HOME/nvim/lua/polirritmico/"
+
 require("polirritmico.settings")
 require("polirritmico.remap")
 require("polirritmico.packer")
--- .config/nvim/lua/polirritmico/remap.lua
+++ .config/nvim/lua/polirritmico/remap.lua
@@ -10,21 +10,13 @@ local function map(mode, key, command)
 	vim.keymap.set(mode, key, command, {silent = true})
 end
 
-local function autocmd(filetype, cmd)
-    vim.api.nvim_create_autocmd(
-        {"FileType"},
-        {pattern = filetype, command = cmd}
-    )
-end
-
-
 --------------
 -- Mappings --
 --------------
 
 -- Teclas líder
 g.mapleader = " "
-g.maplocalleader = "."
+g.maplocalleader = ","
 
 -- Comandos a ñ (misma posición ANSI)
 set({"n", "v"}, "ñ", ":")
@@ -33,8 +25,46 @@ set({"n", "v"}, "Ñ", ";")
 -- Fix goto mark (no reconoce la tecla ` en teclado español)
 map("n", "<bar>", "`")
 
+-- Mover líneas seleccionadas
+map("x", "J", ":m '>+1<CR>gv=gv")
+map("x", "K", ":m '<-2<CR>gv=gv")
+
+-- Mantener posición del cursor con J
+map("n", "J", "mzJ`z")
+
+-- Moverse entre buffers:
+map("n", "<leader>l", ":bnext<CR>")
+map("n", "<leader>h", ":bprevious<CR>")
+map("n", "<leader>db", ":bp<bar>sp<bar>bn<bar>bd<CR>")
+
+-- Regresar al archivo anterior "go back"
+map("n", "<leader>gb", "<C-^>")
+
+-- Centrar vista al hacer scroll
+map("n", "<C-d>", "<C-d>zz")
+map("n", "<C-u>", "<C-u>zz")
+
+-- Centrar vista al hacer búsquedas
+map("n", "n", "nzzzv")
+map("n", "N", "Nzzzv")
+
+-- Registros y clipboard del sistema
+map({"n", "v"}, "<leader>y", "\"+y")        -- Copia al clipboard del sistema
+map("x", "<leader>p", "\"_dP")              -- Pegar sin borrar el registro
+map({"n", "v"}, "<leader>p", "o<ESC>\"+p")  -- Pegar de " en nueva línea
+map({"n", "v"}, "<leader>P", "O<ESC>\"+p")  -- Pegar de " en nueva línea anterior
+
 -- Navegador de archivos
 set("n", "<leader>fe", vim.cmd.Ex)
+
+-- Evitar entrar Ex mode
+map("n", "Q", "")
+
+-- Atajos a configuraciones
+map("n", "<leader>CM", ":e" .. MyConfigPath .. "remap.lua<CR>")
+map("n", "<leader>CP", ":e" .. MyConfigPath .. "packer.lua<CR>")
+map("n", "<leader>CS", ":e" .. MyConfigPath .. "settings.lua<CR>")
+map("n", "<leader>CU", ":e" .. MyConfigPath .. "snips/<CR>")
 
 -- Cambiar dirección de las flechas en los wildmenu (prompt de nvim)
 vim.cmd [[
@@ -49,33 +79,6 @@ vim.cmd [[
                 \ " \<bs>\<C-Z>" : "\<right>"
 ]]
 
--- Moverse entre buffers:
-map("n", "<leader>l", ":bnext<CR>")
-map("n", "<leader>h", ":bprevious<CR>")
-map("n", "<leader>db", ":bp<bar>sp<bar>bn<bar>bd<CR>")
-
--- Regresar al archivo anterior "go back"
-map("n", "<leader>gb", "<C-^>")
-
--- Centrar vista al hacer scroll
-map("n", "<C-d>", "<C-d>zz")
-map("n", "<C-u>", "<C-u>zz")
-
--- Registros y clipboard del sistema
--- Info detallada en :help registers
-map({"n", "v"}, "<leader>y", '"+y')        -- Copia al clipboard del sistema
-map({"n", "v"}, "<leader>p", 'o<ESC>"+p')  -- Copia en nueva línea
-map({"n", "v"}, "<leader>P", 'O<ESC>"+p')  -- Copia en nueva línea anterior
-
-
--- Atajos a configuraciones
-local config_path = " $XDG_CONFIG_HOME/nvim/lua/polirritmico/"
-map("n", "<leader>CM", ":e" .. config_path .. "remap.lua<CR>")
-map("n", "<leader>CP", ":e" .. config_path .. "packer.lua<CR>")
-map("n", "<leader>CS", ":e" .. config_path .. "settings.lua<CR>")
-map("n", "<leader>CU", ":e" .. config_path .. "snips/<CR>")
-
--- Funciones
 -- Guardar y cargar sesión
 exec("exec 'nnoremap <Leader>ss :mks! ' . "..
      "g:sessions_dir . '/*.vim<C-D><BS><BS><BS><BS><BS>'", true)
