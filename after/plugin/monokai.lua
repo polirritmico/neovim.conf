5df4b2e7f00617711255a2b729d9eeb85e73a1ec
polirritmico <ebray187@gmail.com>
2023-08-03 04:33:03 -0400

    Autoupdate: 'Thu Aug  3 04:33:02 -04 2023'

--- after/plugin/monokai.lua
+++ after/plugin/monokai.lua
@@ -7,7 +7,6 @@ vim.opt.cursorline = true
 require("monokai").setup({
     transparent = true,
     lualine_bold = true,
-    day_brightness = 0.3,
     styles = {
         keywords = { italic = false },
     },
@@ -17,13 +16,4 @@ require("monokai").setup({
 vim.cmd("colorscheme monokai")
 
 -- Añadir atajo para conmutar tema oscuro/claro
-local toggle_theme = function()
-    local background = (vim.o.background == "dark") and "light" or "dark"
-    if background == "dark" then
-        require("monokai").setup({transparent = true})
-    else
-        require("monokai").setup({transparent = false})
-    end
-    vim.o.background = background
-end
-Keymap({"n"}, "<leader>tc", toggle_theme, "Monokai: Toggle dark/light theme")
+Keymap({"n"}, "<leader>tc", function() print("Toggle theme") end, "Monokai: Toggle dark/light theme")
