7cd3fb305707742cb51c2db63677dfec77976b22
polirritmico <ebray187@gmail.com>
2024-01-22 02:23:04 -0300

    Autoupdate: 'Mon Jan 22 02:23:03 -03 2024'

--- lua/polirritmico/plugins/ui/gitsigns.lua
+++ lua/polirritmico/plugins/ui/gitsigns.lua
@@ -1,6 +1,4 @@
 --- Git: Highlight code changes from last commit
-local map = require(MyUser .. ".utils").set_keymap
-
 return {
   "lewis6991/gitsigns.nvim",
   event = { "BufReadPost", "BufWritePost", "BufNewFile" },
@@ -22,49 +20,31 @@ return {
     show_deleted = false,
     word_diff = false, -- Toggle with `:Gitsigns toggle_word_diff`
     attach_to_untracked = false,
+    -- stylua: ignore
     on_attach = function(buffer)
       local gs = require("gitsigns")
-      local function map(mode, lhs, rhs, desc)
-        vim.keymap.set(mode, lhs, rhs, { buffer = buffer, desc = desk })
+      local function cmap(mode, lhs, rhs, desc)
+        vim.keymap.set(mode, lhs, rhs, { buffer = buffer, desc = desc })
       end
+
       local function toggle_gitsigns()
         gs.toggle_deleted()
         gs.toggle_current_line_blame()
         gs.toggle_word_diff()
       end
-
-      map("n", "<leader>gt", toggle_gitsigns, "GitSigns: Toggle show deleted lines")
-      map("n", "<leader>gn", gs.next_hunk, "GitSigns: Next file change")
-      map("n", "<leader>gp", gs.prev_hunk, "GitSigns: Previous file change")
-      map("n", "<leader>gsb", gs.stage_buffer, "GitSigns: Stage buffer")
-      map(
-        { "n", "v" },
-        "<leader>gsh",
-        ":Gitsigns stage_hunk<CR>",
-        "GitSigns: Stage hunk"
-      )
-      map(
-        { "n", "v" },
-        "<leader>grh",
-        ":Gitsigns reset_hunk<CR>",
-        "GitSigns: Reset hunk"
-      )
-      map("n", "<leader>gu", gs.undo_stage_hunk, "GitSigns: Undo stage hunk")
-      map("n", "<leader>grb", gs.reset_buffer, "GitSigns: Reset buffer")
-      map("n", "<leader>gP", gs.preview_hunk, "GitSigns: Preview hunk")
-      map("n", "<leader>ga", function()
-        gs.blame_line({ full = true })
-      end, "GitSigns: Blame line")
-      map(
-        "n",
-        "<leader>gc",
-        gs.toggle_current_line_blame,
-        "GitSigns: Toggle current line blame"
-      )
-      map("n", "<leader>gd", gs.diffthis, "GitSigns: Diff this")
-      map("n", "<leader>gD", function()
-        gs.diffthis("~")
-      end, "GitSigns: Diff this")
+      cmap("n", "<leader>gt", toggle_gitsigns, "GitSigns: Toggle show deleted lines")
+      cmap("n", "<leader>gn", gs.next_hunk, "GitSigns: Next file change")
+      cmap("n", "<leader>gp", gs.prev_hunk, "GitSigns: Previous file change")
+      cmap("n", "<leader>gsb", gs.stage_buffer, "GitSigns: Stage buffer")
+      cmap({ "n", "v" }, "<leader>gsh", ":Gitsigns stage_hunk<CR>", "GitSigns: Stage hunk")
+      cmap({ "n", "v" }, "<leader>grh", ":Gitsigns reset_hunk<CR>", "GitSigns: Reset hunk")
+      cmap("n", "<leader>gu", gs.undo_stage_hunk, "GitSigns: Undo stage hunk")
+      cmap("n", "<leader>grb", gs.reset_buffer, "GitSigns: Reset buffer")
+      cmap("n", "<leader>gP", gs.preview_hunk, "GitSigns: Preview hunk")
+      cmap("n", "<leader>ga", function() gs.blame_line({ full = true }) end, "GitSigns: Blame line")
+      cmap("n", "<leader>gc", gs.toggle_current_line_blame, "GitSigns: Toggle current line blame")
+      cmap("n", "<leader>gd", gs.diffthis, "GitSigns: Diff this")
+      cmap("n", "<leader>gD", function() gs.diffthis("~") end, "GitSigns: Diff this")
     end,
   },
 }
