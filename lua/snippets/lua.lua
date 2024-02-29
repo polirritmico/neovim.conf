57822accb2157f186952a879011db17468e64007
polirritmico <ebray187@gmail.com>
2024-02-29 19:35:46 -0300

    Autoupdate: 'Thu Feb 29 19:35:45 -03 2024'

--- lua/snippets/lua.lua
+++ lua/snippets/lua.lua
@@ -128,18 +128,18 @@ return {
     }
   ),
 
-  s(
-    {
-      trig = "---",
-      name = "Horizontal separator line",
-      dscr = "Add a comment line separator.",
-    },
+  s({
+    trig = "---",
+    name = "Horizontal separator line",
+    dscr = "Add a comment line separator.",
+  }, {
     f(function()
       local col = vim.api.nvim_win_get_cursor(0)[2]
       local width = vim.api.nvim_get_option("textwidth") - col - 1
       return string.rep("-", width)
-    end)
-  ),
+    end),
+    t({ "", "" }),
+  }),
 
   s(
     { trig = "lazyenterevent", dscr = "Add common lazy enter buffer event" },
