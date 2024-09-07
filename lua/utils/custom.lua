0fc0a5c8ac5e8de9767e47622fa1d7b574828018
polirritmico <ebray187@gmail.com>
2024-09-07 18:21:29 -0400

    Autoupdate: 'Sat Sep  7 18:21:28 -04 2024'

--- lua/utils/custom.lua
+++ lua/utils/custom.lua
@@ -3,15 +3,7 @@
 local Custom = {}
 
 local api = vim.api
-
----Copies the _last edited_ register (`".`) to the `"d` register and simulates
----a macro recording for use with `Q`.
-function Custom.dot_to_register()
-  vim.cmd([[
-    let reg_val = getreg('.') | normal! qdq
-    call setreg('d', 'ciw' . reg_val . "\<Esc>")
-  ]])
-end
+local ts = vim.treesitter
 
 ---Function used to set a custom text when called by a fold action like zc.
 ---To set it check `:h v:lua-call` and `:h foldtext`.
