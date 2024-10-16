ff0daba575e8bda8425ee3aa5f29c0c6e8765ada
polirritmico <ebray187@gmail.com>
2024-10-15 22:45:53 -0300

    Autoupdate: 'Tue Oct 15 22:45:49 -03 2024'

--- after/ftplugin/tex.lua
+++ after/ftplugin/tex.lua
@@ -1,11 +1,12 @@
-local u = require("utils")
+vim.bo.filetype = "latex"
 
 vim.opt.tabstop = 2
 vim.opt.shiftwidth = 2
-vim.opt.expandtab = true
 vim.bo.softtabstop = 2
-vim.bo.textwidth = 0
+vim.bo.textwidth = 80
 
-vim.opt_local.formatoptions = vim.opt_local.formatoptions + "r" - "o"
+vim.opt_local.formatoptions = vim.opt_local.formatoptions + "a" - "o"
+
+local u = require("utils")
 
 u.config.set_ft_keymap("n", "<leader>mL", u.writing.lorem, "Generate lorem ipsum text")
