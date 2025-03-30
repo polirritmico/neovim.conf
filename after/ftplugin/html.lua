902e38843282801965eea1c120c7c3f1f286eb57
polirritmico <ebray187@gmail.com>
2025-03-30 19:04:42 -0300

    Autoupdate: 'dom 30 mar 2025 19:04:41 -03'

--- after/ftplugin/html.lua
+++ after/ftplugin/html.lua
@@ -1,5 +1,8 @@
 vim.opt.tabstop = 2
 vim.opt.shiftwidth = 2
 vim.opt.expandtab = true
-vim.bo.softtabstop = 2
 vim.opt.textwidth = 0
+
+vim.bo.softtabstop = 2
+vim.bo.smartindent = true
+vim.bo.cindent = false
