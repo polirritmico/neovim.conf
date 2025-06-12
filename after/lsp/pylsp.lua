4c5ecef41917a6806f96eb400fa590776ec5efd4
polirritmico <ebray187@gmail.com>
2025-06-11 23:59:12 -0400

    Autoupdate: 'mié 11 jun 2025 23:59:12 -04'

--- after/lsp/pylsp.lua
+++ after/lsp/pylsp.lua
@@ -1,19 +1,12 @@
 --- https://github.com/python-lsp/python-lsp-server/blob/develop/CONFIGURATION.md
 return {
-  -- cmd = { "pylsp" },
-  -- filetypes = { "python" },
-  -- root_markers = {
-  --   "pyproject.toml",
-  --   "setup.py",
-  --   "setup.cfg",
-  --   "requirements.txt",
-  --   "Pipfile",
-  --   ".git",
-  -- },
   settings = {
     pylsp = {
       plugins = {
         black = { enabled = true },
+        jedi = {
+          extra_paths = { "./src" },
+        },
         pylsp_mypy = { enabled = true },
         pycodestyle = {
           maxLineLength = 88,
